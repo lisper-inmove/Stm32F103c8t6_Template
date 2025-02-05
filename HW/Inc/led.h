d1383033de4a5263a14421bcfe8c14361d75f0b9
inmove <lisper.inmove@gmail.com>
2025-02-05 13:13:47 +0800

    remove

--- HW/Inc/led.h
+++
@@ -1,4 +0,0 @@
-#ifndef __LED_H
-#define __LED_H
-void ToggleLedGPIOB_0(void);
-#endif // !__LED_H
--- HW/Inc/rcc.h
+++
@@ -1,7 +0,0 @@
-#ifndef __RCC__H
-#define __RCC__H
-
-void RccClockInit(void);
-void RccClockInitOuter(void);
-
-#endif
