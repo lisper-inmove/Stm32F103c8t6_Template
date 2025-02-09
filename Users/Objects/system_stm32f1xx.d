ff420560ffd116e5d1d6ec382fa9b913490a0118
inmove <lisper.inmove@gmail.com>
2025-02-09 16:19:38 +0800

    template_v2

---
+++ Users/Objects/system_stm32f1xx.d
@@ -0,0 +1,9 @@
+.\objects\system_stm32f1xx.o: ..\CMSIS\Source\system_stm32f1xx.c
+.\objects\system_stm32f1xx.o: ..\CMSIS\Include\stm32f1xx.h
+.\objects\system_stm32f1xx.o: ..\CMSIS\Include\stm32f103xb.h
+.\objects\system_stm32f1xx.o: ..\CMSIS\Include\core_cm3.h
+.\objects\system_stm32f1xx.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\system_stm32f1xx.o: ..\CMSIS\Include\cmsis_version.h
+.\objects\system_stm32f1xx.o: ..\CMSIS\Include\cmsis_compiler.h
+.\objects\system_stm32f1xx.o: ..\CMSIS\Include\cmsis_armcc.h
+.\objects\system_stm32f1xx.o: ..\CMSIS\Include\system_stm32f1xx.h
